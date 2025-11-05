711440dc472d4b77a61ab0a5016c05b2ecf7d96c
Lars van Duijkeren <larsvanduijkeren@gmail.com>
2025-11-05 10:23:51 +0100

    cap

--- .config/ci/deploy/deploy.rb
+++ .config/ci/deploy/deploy.rb
@@ -2,8 +2,8 @@
 lock "~> 3.17.0"
 
 # application and repo settings
-set :application, "spotenergie"
-set :repo_url, "git@github.com:Larsvanduijkeren/spot-energie.git"
+set :application, "hetpact"
+set :repo_url, "git@github.com:Larsvanduijkeren/hetpact.git"
 
 # branch = environment
 set :branch, "master"
@@ -11,7 +11,7 @@ set :branch, "master"
 set :deploy_user, "deploy"
 
 # target deployment
-set :deploy_to, "/var/www/html/spotenergie"
+set :deploy_to, "/var/www/html/hetpact"
 
 set :theme_folder, "wp-content/themes/websheriff"
 set :root_folder, "../.."
--- .config/ci/deploy/environments/production.rb
+++ .config/ci/deploy/environments/production.rb
@@ -1,4 +1,4 @@
-server "188.166.63.22", roles: %w{app}, name: "production", user: fetch(:deploy_user), port: 222
+server "167.99.217.59", roles: %w{app}, name: "production", user: fetch(:deploy_user), port: 222
 
 # login to remote server is done via ssh-key authentication
 # never, ever with password
