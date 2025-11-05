40460f0bbe4671f8aa96036dda587c9e78cdf458
Lars <info@websheriff.nl>
2025-11-05 10:35:48 +0100

    footer and bugfix

--- wp-content/themes/websheriff/footer.php
+++ wp-content/themes/websheriff/footer.php
@@ -1,8 +1,30 @@
 </main>
 
+<?php
+$footer_logo = get_field('footer_logo', 'option');
+$footer_text_1 = get_field('footer_text_1', 'option');
+$footer_text_2 = get_field('footer_text_2', 'option');
+?>
+
 <footer class='footer'>
     <div class="container">
-        <h2>Footer</h2>
+        <?php if (empty($footer_logo) === false) : ?>
+            <img class="logo" src="<?php echo $footer_logo['sizes']['medium']; ?>" alt="<?php echo $footer_logo['alt']; ?>">
+        <?php endif; ?>
+
+        <div class="flex-wrapper">
+            <div class="col">
+                <?php if(empty($footer_text_1) === false) {
+                    echo $footer_text_1;
+                } ?>
+            </div>
+
+            <div class="col">
+            <?php if(empty($footer_text_2) === false) {
+                echo $footer_text_2;
+            } ?>
+            </div>
+        </div>
     </div>
 </footer>
 
